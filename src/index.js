45939ff266414d48cd2b83c4655cc0a1d8e28858
Viktor Kirichok <viktor.kirichok@gmail.com>
2020-09-08 20:29:12 +0300

    started React

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const image = React.createElement('img', {
+  src:
+    'https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640',
+  alt: 'Tacos With Lime',
+  width: 640,
+});
+const title = React.createElement('h2', null, 'Tacos With Lime');
+const price = React.createElement('p', null, 'Price: 10.99$');
+const button = React.createElement('button', { type: 'button' }, 'Add to cart');
+
+const product = React.createElement('div', null, image, title, price, button);
+
+
+
+ReactDOM.render(product, document.getElementById('root'));

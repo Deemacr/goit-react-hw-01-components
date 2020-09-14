fd9579311eba9df29e4b82fa95d9c483938caeb9
Viktor Kirichok <viktor.kirichok@gmail.com>
2020-09-14 18:43:02 +0300

    Profile and Statistic added

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -2,31 +2,80 @@ import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
 
+const CardContainer = styled.div`
+	max-width: 250px;
+	max-height: 500px;
+	margin: auto;
+	margin-bottom: 20px;
+`;
+
+const ProfileContainer = styled.div`
+	max-width: 250px;
+	background-color: rgb(255, 255, 255);
+	text-align: center;
+	border-color: rgb(0, 0, 0);
+	border-width: 2px;
+`;
+
+const ProfileImage = styled.img`
+	border-radius: 50%;
+	margin: auto;
+	margin-top: 10px;
+`;
+
+const ProfileName = styled.p`
+	font-weight: bolder;
+`;
+
+const ProfileLocation = styled.p`
+	margin-bottom: 0px;
+	padding-bottom: 20px;
+`;
+
+const StatsContainer = styled.ul`
+	display: flex;
+	text-align: center;
+	margin-top: 0px;
+	padding-left: 0px;
+	background-color: #ffffff;
+`;
+
+const StatsItem = styled.li`
+	display: inline-block;
+	border: 1px solid #e4e9f0;
+	padding: 10px;
+`;
+const StatsLabel = styled.span``;
+
+const StatsQuantity = styled.span`
+	font-weight: bolder;
+`;
+
 const Profile = ({ avatar, name, tag, location, stats }) => {
 	return (
-		<div class="profile">
-			<div class="description">
-				<img src={avatar} alt="user avatar" class="avatar" />
-				<p class="name">{name}</p>
-				<p class="tag">{tag}</p>
-				<p class="location">{location}</p>
-			</div>
+		<CardContainer class="profile">
+			<ProfileContainer class="description">
+				<ProfileImage src={avatar} alt="user avatar" class="avatar" />
+				<ProfileName class="name">{name}</ProfileName>
+				<p class="tag">@{tag}</p>
+				<ProfileLocation class="location">{location}</ProfileLocation>
+			</ProfileContainer>
 
-			<ul class="stats">
-				<li>
-					<span class="label">Followers</span>
-					<span class="quantity">{stats.followers}</span>
-				</li>
-				<li>
-					<span class="label">Views</span>
-					<span class="quantity">{stats.views}</span>
-				</li>
-				<li>
-					<span class="label">Likes</span>
-					<span class="quantity">{stats.likes}</span>
-				</li>
-			</ul>
-		</div>
+			<StatsContainer class="stats">
+				<StatsItem>
+					<StatsLabel class="label">Followers </StatsLabel>
+					<StatsQuantity class="quantity">{stats.followers}</StatsQuantity>
+				</StatsItem>
+				<StatsItem>
+					<span class="label">Views </span>
+					<StatsQuantity class="quantity">{stats.views}</StatsQuantity>
+				</StatsItem>
+				<StatsItem>
+					<span class="label">Likes </span>
+					<StatsQuantity class="quantity">{stats.likes}</StatsQuantity>
+				</StatsItem>
+			</StatsContainer>
+		</CardContainer>
 	);
 };
 
--- src/components/Statistics.js
+++ src/components/Statistics.js
@@ -2,18 +2,49 @@ import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
 
+const randomColor = () => {
+	return Math.floor(Math.random() * 16777215).toString(16);
+};
+
+const DataContainer = styled.section`
+	margin: auto;
+	max-width: 250px;
+	text-align: center;
+	background-color: #ffffff;
+`;
+const StatisticalTitle = styled.h2`
+	padding-top: 15px;
+`;
+const StatisticalDataContainer = styled.ul`
+	display: flex;
+	text-align: center;
+	background-color: #ffffff;
+	padding: 0px;
+`;
+
+const StatisticalDataItem = styled.li`
+	display: inline-block;
+	margin: auto;
+	flex: 1 1 auto;
+	width: 20%;
+	color: #ffffff;
+`;
+
 const Statistics = ({ title, stats }) => {
 	return (
-		<section class="statistics">
-			<h2 class="title">{title}</h2>
-			<ul class="stat-list">
+		<DataContainer class="statistics">
+			<StatisticalTitle class="title">{title}</StatisticalTitle>
+			<StatisticalDataContainer class="stat-list">
 				{stats.map((stat) => (
-					<li key={stat.id}>
+					<StatisticalDataItem
+						key={stat.id}
+						style={{ backgroundColor: "#" + randomColor() }}
+					>
 						<p>{stat.label}</p> <p>{stat.percentage}</p>
-					</li>
+					</StatisticalDataItem>
 				))}
-			</ul>
-		</section>
+			</StatisticalDataContainer>
+		</DataContainer>
 	);
 };
 
